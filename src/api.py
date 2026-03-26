9badd348e3ecb0529fc6a647ac6b3c1bdab77c31
RFireforge <raistlinfireforge@hotmail.com>
2026-03-26 12:54:54 +0100

    Cambios api v1

--- src/api.py
+++ src/api.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from src.core.generador import generar_password
 
 app = FastAPI()
 
@@ -7,5 +8,20 @@ def inicio():
     return {"mensaje": "API funcionando 🚀"}
 
 @app.get("/generar")
-def generar():
-    return {"password": "test1234"}
+def generar(
+    longitud: int = Query(16, ge=8, le=128, description="Longitud de la contraseña"),
+    incluir_mayus: bool = Query(True, description="Incluir mayúsculas"),
+    incluir_minus: bool = Query(True, description="Incluir minúsculas"),
+    incluir_numeros: bool = Query(True, description="Incluir números"),
+    incluir_simbolos: bool = Query(True, description="Incluir símbolos"),
+    usuario: str = Query("", description="Nombre del usuario para evitarlo en la contraseña")
+):
+    password = generar_password(
+        longitud=longitud,
+        incluir_mayus=incluir_mayus,
+        incluir_minus=incluir_minus,
+        incluir_numeros=incluir_numeros,
+        incluir_simbolos=incluir_simbolos,
+        usuario=usuario
+    )
+    return {"password": password}
